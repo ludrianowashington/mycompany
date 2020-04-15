c49992963d32c2d79d6d6a7a1b62e5e3076b9b60
ludrianowashington <ludriano.washi@gmail.com>
2020-04-15 04:32:59 -0300

    Novo commit e alteração do javascript

--- index.js
+++ index.js
@@ -1,45 +1,24 @@
-window.onload = initialCash();
+window.onload = changeValue();
 
-let box = document.querySelector("#cash");
-box.addEventListener("click", function (event) {
-  actualizeTotalCash();
-});
+/**
+ *  Função que atualiza o Valor Total em Caixa
+ */
 
 function setTotalCash(value) {
-  let totalCash = document.getElementById("totalCash").innerText;
+  let totalCash = document.getElementById("cash");
 
-  let val = convertToFloat(value).toLocaleString("pt-BR", {
-    minimumFractionDigits: 2,
-  });
-  console.log(val);
-  return (totalCash.innerText = val);
+  return (totalCash.innerText = value);
+  window.location.reload(true);
 }
 
-function convertToString(val) {
-  return val.toString();
-}
+/**
+ *  Função que carrega o necessário para a aplicação
+ */
 
-function convertToInt(val) {
-  return parseInt(val);
-}
+function changeValue() {
+  let value = "100,00";
 
-function convertToFloat(value) {
-  return parseFloat(value);
-}
+  setTotalCash(value);
 
-function initialCash() {
-  let cash = "100,00";
-  let val = convertToFloat(cash).toLocaleString("pt-BR", {
-    minimumFractionDigits: 2,
-  });
-  return setTotalCash(val);
-}
-
-function actualizeTotalCash() {
-  let currentValue = convertToFloat(
-    document.getElementById("totalCash").innerText
-  );
-  let newValue = currentValue + 0.1;
-  console.log(currentValue);
-  return setTotalCash(newValue);
+  return data;
 }
