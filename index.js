832d9c77caa46ce47f87f104e65e00ce94a797d5
ludrianowashington <ludriano.washi@gmail.com>
2020-04-17 05:07:48 -0300

    Criação de função para criar telas no main

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.onload = changeValue();
+window.onload = initialization();
 
 /**
  *  Função que atualiza o Valor Total em Caixa
@@ -8,7 +8,7 @@ function setTotalCash(value) {
   let totalCash = document.getElementById("cash");
 
   return (totalCash.innerText = value);
-  window.location.reload(true);
+  // window.location.reload(false);
 }
 
 /**
@@ -19,6 +19,39 @@ function changeValue() {
   let value = "100,00";
 
   setTotalCash(value);
-
   return data;
+}
+
+function initialization() {
+  newMain();
+  changeValue();
+}
+
+function newMain() {
+  let newMain = document.getElementById("main");
+  // Criar uma caixa para adição de itens
+  let component = document.createElement("div");
+  component.id = "box-content";
+  component.className = "box-container";
+
+  newMain.appendChild(component);
+  menuBar();
+}
+
+function newComp(link, id) {
+  let linka = link;
+  let inf = id;
+  let comp = document.createElement("p");
+  linka.appendChild(comp);
+}
+
+function menuBar(view) {
+  let linkado = document.querySelector("#box-content");
+  let links = document.querySelectorAll(".link");
+  let test;
+  let i;
+  for (i = 0; i < links.length; i++) {
+    test = links.item(i).id;
+    return newComp(linkado, test);
+  }
 }
