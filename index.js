3f83cb6b4fa8c54c34065188f9edde5c29876d7d
ludrianowashington <ludriano.washi@gmail.com>
2020-04-20 01:50:17 -0300

    Criação de manipulador de links do menu

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+// Iniciando quando a janela é carregada
+
 window.onload = initialization();
+
+/**
+ *  Função que carrega o necessário para a apliação
+ */
+
+function initialization() {
+  newMain();
+  changeValue();
+  handleClick();
+}
 
 /**
  *  Função que atualiza o Valor Total em Caixa
@@ -6,52 +18,72 @@ window.onload = initialization();
 
 function setTotalCash(value) {
   let totalCash = document.getElementById("cash");
-
   return (totalCash.innerText = value);
-  // window.location.reload(false);
 }
 
 /**
- *  Função que carrega o necessário para a aplicação
+ *  Função que inicializa Valor Total em caixa
  */
 
 function changeValue() {
   let value = "100,00";
 
   setTotalCash(value);
-  return data;
-}
-
-function initialization() {
-  newMain();
-  changeValue();
 }
 
 function newMain() {
   let newMain = document.getElementById("main");
+
   // Criar uma caixa para adição de itens
   let component = document.createElement("div");
   component.id = "box-content";
   component.className = "box-container";
-
   newMain.appendChild(component);
-  menuBar();
 }
 
-function newComp(link, id) {
-  let linka = link;
-  let inf = id;
-  let comp = document.createElement("p");
-  linka.appendChild(comp);
+function handleClick() {
+  const purchasing = document.querySelector("#purchasing");
+  const manufacturing = document.querySelector("#manufacturing");
+  const inventory = document.querySelector("#inventory");
+  const sales = document.querySelector("#sales");
+  const automation = document.querySelector("#automation");
+  const data = document.querySelector("#data");
+
+  purchasing.addEventListener("click", message1);
+  manufacturing.addEventListener("click", message2);
+  inventory.addEventListener("click", message3);
+  sales.addEventListener("click", message4);
+  automation.addEventListener("click", message5);
+  data.addEventListener("click", message6);
 }
 
-function menuBar(view) {
-  let linkado = document.querySelector("#box-content");
-  let links = document.querySelectorAll(".link");
-  let test;
-  let i;
-  for (i = 0; i < links.length; i++) {
-    test = links.item(i).id;
-    return newComp(linkado, test);
-  }
+function message1() {
+  const msg = document.querySelector(".box-container");
+
+  msg.innerHTML = "Compra";
+}
+function message2() {
+  const msg = document.querySelector(".box-container");
+
+  msg.innerHTML = "Fabricaçao";
+}
+function message3() {
+  const msg = document.querySelector(".box-container");
+
+  msg.innerHTML = "Inventário";
+}
+function message4() {
+  const msg = document.querySelector(".box-container");
+
+  msg.innerHTML = "Vendas";
+}
+function message5() {
+  const msg = document.querySelector(".box-container");
+
+  msg.innerHTML = "Automação";
+}
+function message6() {
+  const msg = document.querySelector(".box-container");
+
+  msg.innerHTML = "Dados";
 }
